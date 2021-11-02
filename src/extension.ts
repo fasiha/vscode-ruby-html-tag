97a452589dadf977c1784738fe4770f550a285fb
Ahmed Fasih <wuzzyview@gmail.com>
2021-11-01 18:24:43 -0700

    add ruby/rt tags to selection

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,31 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// https://github.com/microsoft/vscode-extension-samples/blob/main/document-editing-sample/src/extension.ts
+// https://stackoverflow.com/questions/65248766/does-vs-code-extensions-api-support-changing-cursor-position
+// https://code.visualstudio.com/api/references/commands --> `cursorMove`
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors
-  // (console.error) This line of code will only be executed once when your
-  // extension is activated
-  console.log('Congratulations, your extension "ruby-html-tag" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable =
-      vscode.commands.registerCommand('ruby-html-tag.helloWorld', () => {
-        // The code you place here will be executed every time your command is
-        // executed Display a message box to the user
-        vscode.window.showInformationMessage('Hello pooper');
+  let disposable = vscode.commands.registerCommand(
+      'ruby-html-tag.addRubyRtTags', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+          const document = editor.document;
+          const selection = editor.selection;
+          const text = document.getText(selection);
+          editor.edit(editBuilder => {
+            editBuilder.replace(
+                selection, `<ruby>${text}<rt>RUBY TEXT</rt></ruby>`);
+          });
+          await vscode.commands.executeCommand(
+              'cursorMove', {to: 'right', by: 'character', value: 1});
+          await vscode.commands.executeCommand(
+              'cursorMove', {to: 'left', by: 'character', value: 12});
+          await vscode.commands.executeCommand(
+              'cursorMove',
+              {to: 'left', by: 'character', value: 9, select: true});
+        }
       });
 
   context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
 export function deactivate() {}
